fc063adb05092f9215644a18ec3517020529206a
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2026-06-16 00:13:38 +0500

    Apply Komikku methods and remove deprecated APIs

--- library/src/main/java/eu/kanade/tachiyomi/source/CatalogueSource.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/CatalogueSource.kt
@@ -56,4 +56,36 @@ interface CatalogueSource : Source {
      */
     @Deprecated("Use the suspend API instead", ReplaceWith("getLatestUpdates"))
     fun fetchLatestUpdates(page: Int): Observable<MangasPage> = throw Exception("Stub!")
+
+    /**
+     * Whether parsing related mangas in manga page or extension provide custom related mangas request.
+     * Only Supported on Komikku
+     * @default false
+     */
+    val supportsRelatedMangas: Boolean get() = false
+
+    /**
+     * Extensions doesn't want to use App's [getRelatedMangaListBySearch].
+     * Only Supported on Komikku
+     * @default false
+     */
+    val disableRelatedMangasBySearch: Boolean get() = false
+
+    /**
+     * Disable showing any related titles.
+     * Only Supported on Komikku
+     * @default false
+     */
+    val disableRelatedMangas: Boolean get() = false
+
+    /**
+     * Fetch related mangas for a manga from source/site.
+     * Only Supported on Komikku
+     *
+     * @param manga the current manga to get related mangas.
+     * @return the related mangas for the current manga.
+     * @throws UnsupportedOperationException if a source doesn't support related mangas.
+     */
+    suspend fun fetchRelatedMangaList(manga: SManga): List<SManga> = throw UnsupportedOperationException("Unsupported!")
 }
+
